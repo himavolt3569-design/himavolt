e723ab78c9dad8d1600b085c5c9644740d9ab617
Praashon <prashon.social@gmail.com>
2026-04-06 22:10:14 +0545

    chore: remove test-db.ts scratch file

--- test-db.ts
+++
@@ -1,5 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-prisma.restaurant.findMany({ select: { slug: true, name: true } }).then(res => {
-  console.log(JSON.stringify(res, null, 2));
-}).catch(console.error).finally(()=>prisma.$disconnect());
